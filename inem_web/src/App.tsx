fcce9ec51d99a51a01ec8a9f6924fa9d28cb4b0b
Florian Berisha <florian.fani@hotmail.com>
2024-09-28 16:53:04 +0200

    footer update

--- inem_web/src/App.tsx
+++ inem_web/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
+import { Footer } from "./components/Footer";
 function App() {
   return (
     <div>
@@ -11,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
+        <div>
+          <Footer />
+        </div>
       </Router>
     </div>
   );
