ab18427bf3da28dbde65407ea95b7821156d15af
ArtisAdemi <artis.ademi@gmail.com>
2024-09-29 12:05:32 +0200

    WhatWeDo

--- inem_web/src/pages/HomePage.tsx
+++ inem_web/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import Construction from '../assets/Construction.jpeg'
+import WhatWeDo from '../components/WhatWeDo'
 
 const HomePage = () => {
     return (
         <div className="w-full h-2screen">
             <img src={Construction} alt="hero image" className="w-full h-full object-cover" />
+            <WhatWeDo />
         </div>
     )
 }
