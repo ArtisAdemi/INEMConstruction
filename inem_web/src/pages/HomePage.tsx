ba7baeadbeb958bce523a76a09a3c57eaaaeb150
Florian Berisha <florian.fani@hotmail.com>
2024-09-30 16:46:02 +0200

    HomeAbout finished

--- inem_web/src/pages/HomePage.tsx
+++ inem_web/src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeSlider from "../components/HomeSlider";
 import WhatWeDo from "../components/WhatWeDo";
+import HomeAbout from "../components/HomeAbout";
 
 const HomePage: React.FC = () => {
   return (
     <div className="w-full h-2screen overflow-hidden">
       <HomeSlider />
+      <HomeAbout />
       <WhatWeDo />
     </div>
   );
