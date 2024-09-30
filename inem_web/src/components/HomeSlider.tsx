4503df6fd3bfe7694cf884b0e6ca24573b9e0a40
Florian Berisha <florian.fani@hotmail.com>
2024-09-30 12:48:35 +0200

    slider text overlay fixup

--- inem_web/src/components/HomeSlider.tsx
+++ inem_web/src/components/HomeSlider.tsx
@@ -18,15 +18,15 @@ const HomeSlider: React.FC = () => {
   const sliders = [
     {
       image: Construction,
-      text: "Artis Babuqi in da Club",
+      text: "First Construction Text",
     },
     {
       image: Construction2,
-      text: "Florian Seksi ma i Qarti",
+      text: "Second Construction Text",
     },
     {
       image: Construction3,
-      text: "Labinot Llegenda Kanter strajt",
+      text: "Third Construction Text",
     },
   ];
 
@@ -36,8 +36,8 @@ const HomeSlider: React.FC = () => {
         {sliders.map((slider, index) => (
           <div key={index} className="relative h-screen">
             <img src={slider.image} alt="" className="w-full h-screen object-cover" />
-            <div className="absolute top-[45%] left-[50%] lg:right-[45%] z-50 flex items-center justify-center">
-              <div className="flex-col">
+            <div className="absolute inset-0 flex items-center justify-center z-50">
+              <div className="text-center">
                 <h3 className="text-xl text-white font-semibold mb-2 leading-tight">{slider.text}</h3>
                 <p className="text-white mb-4">{slider.text}</p>
               </div>
