5d1cc2e0735e8ffeb848355a698896e09e96005a
ArtisAdemi <artis.ademi@gmail.com>
2024-10-01 16:37:55 +0200

    Services page

---
+++ inem_web/src/assets/services.ts
@@ -0,0 +1,53 @@
+import project5 from '../assets/project5.jpg';
+import { WhatWeDo1, WhatWeDo2, WhatWeDo3, WhatWeDo4, WhatWeDo5, WhatWeDo6 } from './decorations';
+
+export const services = [
+    {
+        id: 1,
+        title: "ZERO HARM EVERYDAY",
+        slug: "zero-harm-everyday",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo1, // Replace with actual icon
+    },
+    {
+        id: 2,
+        title: "VIRTUAL CONSTRUCTION",
+        slug: "virtual-construction",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo2, // Replace with actual icon
+    },
+    {
+        id: 3,
+        title: "BUILD TO LAST",
+        slug: "build-to-last",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo3, // Replace with actual icon
+    },
+    {
+        id: 4,
+        title: "EXTERIOR DESIGN",
+        slug: "exterior-design",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo4, // Replace with actual icon
+    },
+    {
+        id: 5,
+        title: "RENOVATION",
+        slug: "renovation",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo5, // Replace with actual icon
+    },
+    {
+        id: 6,
+        title: "SAFETY MANAGEMENT",
+        slug: "safety-management",
+        description: "You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit.",
+        image: project5,
+        icon: WhatWeDo6, // Replace with actual icon
+    },
+];
