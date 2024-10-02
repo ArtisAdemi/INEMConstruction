549f8458786966db9d6394941ec271db8ac883da
Florian Berisha <florian.fani@hotmail.com>
2024-10-02 16:29:02 +0200

    button slider added

--- inem_web/src/components/HomeSlider.tsx
+++ inem_web/src/components/HomeSlider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -6,8 +6,21 @@ import twoworkers from "../assets/twoworkers.jpg";
 import Construction from "../assets/Construction.jpeg";
 import Construction2 from "../assets/Construction2.jpeg";
 import Construction3 from "../assets/Construction3.jpeg";
+import arrow from "../assets/decorations/ArrowLeft.svg";
 
 const HomeSlider: React.FC = () => {
+  let sliderRef = useRef<Slider | null>(null);
+  const next = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickNext();
+    }
+  };
+  const previous = () => {
+    if (sliderRef.current) {
+      sliderRef.current.slickPrev();
+    }
+  };
+
   const settings = {
     dots: true,
     infinite: true,
@@ -39,7 +52,7 @@ const HomeSlider: React.FC = () => {
 
   return (
     <div className="slider-wrapper bg-center bg-cover">
-      <Slider {...settings}>
+      <Slider ref={sliderRef} {...settings}>
         {sliders.map((slider, index) => (
           <div key={index} className="relative h-screen">
             <img src={slider.image} alt="" className="w-full h-screen object-cover brightness-50" />
@@ -50,6 +63,14 @@ const HomeSlider: React.FC = () => {
               </div>
               <div className="flex align-bottom items-end">
                 <button className="text-lg py-3 px-8 mt-5 text-white border-2 border-white hover:bg-white hover:duration-500 duration-500 hover:text-[#ff7404]">Learn More</button>
+              </div>
+              <div className="text-center hidden absolute lg:flex justify-between inset-0 w-full overflow-hidden">
+                <button className="button p-3 rounded-lg hover:scale-105 duration-300 bg-transparent text-white font-semibold" onClick={previous}>
+                  <img src={arrow} alt="" />
+                </button>
+                <button className="button !p-3 rounded-lg hover:scale-105 duration-300 bg-transparent text-white font-semibold" onClick={next}>
+                  <img src={arrow} alt="" className="rotate-180 text-white" />
+                </button>
               </div>
             </div>
           </div>
