7e44ce9989ac570b0da26a9f5c107389b97d1167
ArtisAdemi <artis.ademi@gmail.com>
2024-10-06 15:31:32 +0200

    dynamically show Single Project data

--- inem_web/src/pages/SingleProject.tsx
+++ inem_web/src/pages/SingleProject.tsx
@@ -50,22 +50,19 @@ const SingleProject: React.FC = () => {
                     <h1 className="text-4xl font-bold mb-4">{project.title}</h1>
                     <p className="text-gray-700 mb-4">{project.description}</p>
                     <div className="mb-4">
-                        <strong>Client:</strong> Pransbay Powers Authority
+                        <strong>Client:</strong> {project.client}
                     </div>
                     <div className="mb-4">
-                        <strong>Architect:</strong> Diarke Pelli Incorp
+                        <strong>Location:</strong> {project.location}
                     </div>
                     <div className="mb-4">
-                        <strong>Location:</strong> McLean, VA
+                        <strong>Size:</strong> {project.size}
                     </div>
                     <div className="mb-4">
-                        <strong>Size:</strong> 65,000 SF
+                        <strong>Year Completed:</strong> {project.year}
                     </div>
                     <div className="mb-4">
-                        <strong>Year Completed:</strong> 2015
-                    </div>
-                    <div className="mb-4">
-                        <strong>Categories:</strong> Commercial, Interiors
+                        <strong>Categories:</strong> {project.services.join(', ')}
                     </div>
                     <button className="bg-[#439C91] hover:opacity-80 text-white py-2 px-4 rounded" onClick={() => redirect('/contact')}>Contact Us</button>
                 </div>
