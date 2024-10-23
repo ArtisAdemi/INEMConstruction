0fb32b47cd739387a4051cd88edfd46363856f11
Florian <florian.fani@hotmail.com>
2024-10-23 18:27:18 +0200

    footer and HappyClients updated

--- inem_web/src/components/Footer.tsx
+++ inem_web/src/components/Footer.tsx
@@ -19,25 +19,24 @@ export const Footer: React.FC = () => {
       <div className="justify-between xl:flex gap-3 py-[125px] border-b border-[#999999]">
         <div className="lg:w-[30%] w-full">
           <div className="mb-5 items-end">
-            <img className="w-64 cursor-pointer" src={logo} alt="logo image" onClick={() => redirect("/")} />
+            <img className="w-64 cursor-pointer -ml-12 xl:-mt-4" src={logo} alt="logo image" onClick={() => redirect("/")} />
           </div>
-          <p className="text-[#999999] mb-8">We build readymade websites, mobile applications, and elaborate online business services</p>
+          <p className="text-[#999999] mb-8">Neuf ou rénovation - particulier ou professionnel. Nos aptitudes conviendrons à vos projets les plus pointilleux. Prenez contact sans plus attendre et faire de
+            votre rêve une réalité.</p>
         </div>
         <div className="lg:w-[30%] w-full">
           <div className="mb-5 items-end">
-            <h2 className="text-white font-semibold mb-3">WORKING HOURS</h2>
-            <p className="text-[#999999]">We work 7 days a week, every day excluding major holidays. Contact us if you have an emergency, with our Hotline and Contact form.</p>
+            <h2 className="text-white font-semibold mb-3">Horaires d’ouverture</h2>
+            <p className="text-[#999999]">Nous travaillons du lundi au vendredi de 07:00
+              à 18:00 mais également en horaires de nuits ou
+              le week end pour les cas urgents</p>
           </div>
           <div className="flex justify-between">
             <div>
-              <p className="text-[#999999]">Monday-Friday</p>
-              <p className="text-[#999999]">Saturday</p>
-              <p className="text-[#999999]">Sunday and holidays</p>
+              <p className="text-[#999999]">Lundi - Vendredi</p>
             </div>
             <div>
-              <p className="text-[#999999]">10:00 - 16:00</p>
-              <p className="text-[#999999]">12:00 - 15:00</p>
-              <p className="text-[#999999]">09:00 - 12:00</p>
+              <p className="text-[#999999]">07:00 - 18:00</p>
             </div>
           </div>
         </div>
@@ -45,7 +44,7 @@ export const Footer: React.FC = () => {
         <div className="xl:flex gap-3 justify-center md:w-[30%] w-full mt-12 xl:mt-4">
           <div className="flex flex-col gap-y-2">
             <h2 className={`cursor-pointer hover:text-[#439C91] duration-300 ${isActive("/") ? "font-bold text-[#439C91]" : "text-[#999999]"}`} onClick={() => redirect("/")}>
-              {isActive("/") ? "- D'accueil" : "D'accueil"}
+              {isActive("/") ? "- Accueil" : "Accueil"}
             </h2>
             <h2 className={`cursor-pointer hover:text-[#439C91] duration-300 ${isActive("/projects") ? "font-bold text-[#439C91]" : "text-[#999999]"}`} onClick={() => redirect("/projects")}>
               {isActive("/projects") ? "- Nos réalisations" : "Nos réalisations"}
@@ -57,7 +56,7 @@ export const Footer: React.FC = () => {
               {isActive("/about") ? "- Notre histoire" : "Notre histoire"}
             </h2>
             <h2 className={`cursor-pointer hover:text-[#439C91] duration-300 ${isActive("/contact") ? "font-bold text-[#439C91]" : "text-[#999999]"}`} onClick={() => redirect("/contact")}>
-              {isActive("/contact") ? "- Contacte" : "Contacte"}
+              {isActive("/contact") ? "- Nous Contacter" : "Nous Contacter"}
             </h2>
           </div>
         </div>
